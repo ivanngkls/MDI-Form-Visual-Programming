8c25c6b1ea6fc20f0efbe7de239a2e6c21db1d3e
ivanngkls <ivangoklas1234@gmail.com>
2025-03-27 17:37:02 +0700

    Add files via upload

---
+++ TugasPemvisMDI/GUI/Menu_Utama.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace TugasPemvisMDI.GUI
+{
+    partial class Menu_Utama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu_Utama));
+            menuStrip = new MenuStrip();
+            pilihMenuToolStripMenuItem = new ToolStripMenuItem();
+            submenu_stopwatch = new ToolStripMenuItem();
+            submenu_timer = new ToolStripMenuItem();
+            toolStrip = new ToolStrip();
+            newToolStripButton = new ToolStripButton();
+            openToolStripButton = new ToolStripButton();
+            saveToolStripButton = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            printToolStripButton = new ToolStripButton();
+            printPreviewToolStripButton = new ToolStripButton();
+            toolStripSeparator2 = new ToolStripSeparator();
+            helpToolStripButton = new ToolStripButton();
+            statusStrip = new StatusStrip();
+            toolStripStatusLabel = new ToolStripStatusLabel();
+            toolTip = new ToolTip(components);
+            menuStrip.SuspendLayout();
+            toolStrip.SuspendLayout();
+            statusStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { pilihMenuToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Padding = new Padding(7, 2, 0, 2);
+            menuStrip.Size = new Size(737, 24);
+            menuStrip.TabIndex = 0;
+            menuStrip.Text = "MenuStrip";
+            // 
+            // pilihMenuToolStripMenuItem
+            // 
+            pilihMenuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { submenu_stopwatch, submenu_timer });
+            pilihMenuToolStripMenuItem.Name = "pilihMenuToolStripMenuItem";
+            pilihMenuToolStripMenuItem.Size = new Size(76, 20);
+            pilihMenuToolStripMenuItem.Text = "Pilih Menu";
+            // 
+            // submenu_stopwatch
+            // 
+            submenu_stopwatch.Name = "submenu_stopwatch";
+            submenu_stopwatch.Size = new Size(180, 22);
+            submenu_stopwatch.Text = "&Stop Watch";
+            submenu_stopwatch.Click += submenu_stopwatch_Click;
+            // 
+            // submenu_timer
+            // 
+            submenu_timer.Name = "submenu_timer";
+            submenu_timer.Size = new Size(180, 22);
+            submenu_timer.Text = "&Timer";
+            submenu_timer.Click += submenu_timer_Click;
+            // 
+            // toolStrip
+            // 
+            toolStrip.Items.AddRange(new ToolStripItem[] { newToolStripButton, openToolStripButton, saveToolStripButton, toolStripSeparator1, printToolStripButton, printPreviewToolStripButton, toolStripSeparator2, helpToolStripButton });
+            toolStrip.Location = new Point(0, 24);
+            toolStrip.Name = "toolStrip";
+            toolStrip.Size = new Size(737, 25);
+            toolStrip.TabIndex = 1;
+            toolStrip.Text = "ToolStrip";
+            // 
+            // newToolStripButton
+            // 
+            newToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            newToolStripButton.Image = (Image)resources.GetObject("newToolStripButton.Image");
+            newToolStripButton.ImageTransparentColor = Color.Black;
+            newToolStripButton.Name = "newToolStripButton";
+            newToolStripButton.Size = new Size(23, 22);
+            newToolStripButton.Text = "New";
+            newToolStripButton.Click += ShowNewForm;
+            // 
+            // openToolStripButton
+            // 
+            openToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            openToolStripButton.Image = (Image)resources.GetObject("openToolStripButton.Image");
+            openToolStripButton.ImageTransparentColor = Color.Black;
+            openToolStripButton.Name = "openToolStripButton";
+            openToolStripButton.Size = new Size(23, 22);
+            openToolStripButton.Text = "Open";
+            openToolStripButton.Click += OpenFile;
+            // 
+            // saveToolStripButton
+            // 
+            saveToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            saveToolStripButton.Image = (Image)resources.GetObject("saveToolStripButton.Image");
+            saveToolStripButton.ImageTransparentColor = Color.Black;
+            saveToolStripButton.Name = "saveToolStripButton";
+            saveToolStripButton.Size = new Size(23, 22);
+            saveToolStripButton.Text = "Save";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 25);
+            // 
+            // printToolStripButton
+            // 
+            printToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            printToolStripButton.Image = (Image)resources.GetObject("printToolStripButton.Image");
+            printToolStripButton.ImageTransparentColor = Color.Black;
+            printToolStripButton.Name = "printToolStripButton";
+            printToolStripButton.Size = new Size(23, 22);
+            printToolStripButton.Text = "Print";
+            // 
+            // printPreviewToolStripButton
+            // 
+            printPreviewToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            printPreviewToolStripButton.Image = (Image)resources.GetObject("printPreviewToolStripButton.Image");
+            printPreviewToolStripButton.ImageTransparentColor = Color.Black;
+            printPreviewToolStripButton.Name = "printPreviewToolStripButton";
+            printPreviewToolStripButton.Size = new Size(23, 22);
+            printPreviewToolStripButton.Text = "Print Preview";
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(6, 25);
+            // 
+            // helpToolStripButton
+            // 
+            helpToolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            helpToolStripButton.Image = (Image)resources.GetObject("helpToolStripButton.Image");
+            helpToolStripButton.ImageTransparentColor = Color.Black;
+            helpToolStripButton.Name = "helpToolStripButton";
+            helpToolStripButton.Size = new Size(23, 22);
+            helpToolStripButton.Text = "Help";
+            // 
+            // statusStrip
+            // 
+            statusStrip.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel });
+            statusStrip.Location = new Point(0, 501);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Padding = new Padding(1, 0, 16, 0);
+            statusStrip.Size = new Size(737, 22);
+            statusStrip.TabIndex = 2;
+            statusStrip.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            toolStripStatusLabel.Name = "toolStripStatusLabel";
+            toolStripStatusLabel.Size = new Size(39, 17);
+            toolStripStatusLabel.Text = "Status";
+            // 
+            // Menu_Utama
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(737, 523);
+            Controls.Add(statusStrip);
+            Controls.Add(toolStrip);
+            Controls.Add(menuStrip);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Menu_Utama";
+            Text = "Menu Utama";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            toolStrip.ResumeLayout(false);
+            toolStrip.PerformLayout();
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripButton newToolStripButton;
+        private System.Windows.Forms.ToolStripButton openToolStripButton;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private System.Windows.Forms.ToolStripButton printToolStripButton;
+        private System.Windows.Forms.ToolStripButton printPreviewToolStripButton;
+        private System.Windows.Forms.ToolStripButton helpToolStripButton;
+        private System.Windows.Forms.ToolTip toolTip;
+        private ToolStripMenuItem pilihMenuToolStripMenuItem;
+        public ToolStripMenuItem submenu_stopwatch;
+        public ToolStripMenuItem submenu_timer;
+    }
+}
+
+
+
---
+++ TugasPemvisMDI/GUI/formStopwatch.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TugasPemvisMDI.GUI
+{
+    partial class formStopwatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            stopwatch = new Label();
+            lblstopwatch = new Label();
+            mulai_btn = new Button();
+            berhenti_btn = new Button();
+            SuspendLayout();
+            // 
+            // stopwatch
+            // 
+            stopwatch.AutoSize = true;
+            stopwatch.Font = new Font("Arial", 20F);
+            stopwatch.Location = new Point(196, 28);
+            stopwatch.Name = "stopwatch";
+            stopwatch.Size = new Size(141, 32);
+            stopwatch.TabIndex = 0;
+            stopwatch.Text = "Stopwatch";
+            // 
+            // lblstopwatch
+            // 
+            lblstopwatch.AutoSize = true;
+            lblstopwatch.Font = new Font("Arial", 24F);
+            lblstopwatch.Location = new Point(207, 122);
+            lblstopwatch.Name = "lblstopwatch";
+            lblstopwatch.Size = new Size(114, 36);
+            lblstopwatch.TabIndex = 1;
+            lblstopwatch.Text = "00 : 00";
+            lblstopwatch.TextAlign = ContentAlignment.TopRight;
+            // 
+            // mulai_btn
+            // 
+            mulai_btn.BackColor = Color.Lime;
+            mulai_btn.Location = new Point(125, 191);
+            mulai_btn.Name = "mulai_btn";
+            mulai_btn.Size = new Size(100, 40);
+            mulai_btn.TabIndex = 2;
+            mulai_btn.Text = "Mulai";
+            mulai_btn.UseVisualStyleBackColor = false;
+            // 
+            // berhenti_btn
+            // 
+            berhenti_btn.BackColor = Color.Red;
+            berhenti_btn.Location = new Point(319, 191);
+            berhenti_btn.Name = "berhenti_btn";
+            berhenti_btn.Size = new Size(100, 40);
+            berhenti_btn.TabIndex = 3;
+            berhenti_btn.Text = "Berhenti";
+            berhenti_btn.UseVisualStyleBackColor = false;
+            // 
+            // formStopwatch
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(534, 284);
+            Controls.Add(berhenti_btn);
+            Controls.Add(mulai_btn);
+            Controls.Add(lblstopwatch);
+            Controls.Add(stopwatch);
+            Name = "formStopwatch";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label stopwatch;
+        private Label lblstopwatch;
+        private Button mulai_btn;
+        private Button berhenti_btn;
+    }
+}
